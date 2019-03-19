f561da6e98ccf5d887f072a0dcb85bddcddc19dc
Mario <mariokpo0010@gmail.com>
2019-03-19 16:18:58 -0300

    a

---
+++ teoria2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int Buff(5)=(5,15,30,10,35);
+	int *p;
+	p=Buff;
+
+	int elemento;
+	for (int i = 0; i < 5; ++i)
+	{		
+		elemento=Buff(i);
+		printf("%d - ",  elemento);
+	}
+
+	for (int y=o ; i<5 ; i++)
+	{
+		
+	}
+
+}

8d22563c01b62cddf0353ad6645e12f782ff2a57
Mario <mariokpo10@gmail.com>
2019-03-27 02:26:43 -0300

    Subiendo archivo con el codigo

---
+++ tp2_1_1.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 4
+#define M 5
+
+int main(void) 
+{
+	int f,c;
+	float mt [N] [M];
+
+	for(int f = 0 ; f<N ; f++)
+	{
+		for (int f = 0; f < N; f++)
+		{
+			printf("%f  ", mt[f] [c]);
+		}
+		printf("\n");
+
+		return 0;
+	}
+}
